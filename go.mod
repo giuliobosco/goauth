af1c34173523392122d99477270d67d60a5a6953
giuliobosco <giuliobva@gmail.com>
2020-06-06 22:33:03 +0200

    add app

---
+++ go.mod
@@ -0,0 +1,12 @@
+module goauth
+
+go 1.12
+
+require (
+	github.com/boj/redistore v0.0.0-20180917114910-cd5dcc76aeff // indirect
+	github.com/gin-gonic/contrib v0.0.0-20191209060500-d6e26eeaa607
+	github.com/gin-gonic/gin v1.6.3
+	github.com/gorilla/context v1.1.1 // indirect
+	github.com/gorilla/sessions v1.2.0 // indirect
+	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d
+)
---
+++ main.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/contrib/sessions"
+	"github.com/gin-gonic/gin"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+)
+
+type Credentials struct {
+	Cid     string `json:"cid"`
+	Csecret string `json:"csecret"`
+}
+
+var cred Credentials
+var conf *oauth2.Config
+var state string
+var store = sessions.NewCookieStore([]byte("secret"))
+
+type User struct {
+	Sub           string `json:"sub"`
+	Name          string `json:"name"`
+	GivenName     string `json:"given_name"`
+	FamilyName    string `json:"family_name"`
+	Profile       string `json:"profile"`
+	Picture       string `json:"picture"`
+	Email         string `json:"email"`
+	EmailVerified string `json:"email_verified"`
+	Gender        string `json:"gender"`
+}
+
+func randToken() string {
+	b := make([]byte, 32)
+	rand.Read(b)
+	return base64.StdEncoding.EncodeToString(b)
+}
+
+func init() {
+	file, err := ioutil.ReadFile("./creds.json")
+	if err != nil {
+		log.Printf("File error: %v\n", err)
+		os.Exit(1)
+	}
+	json.Unmarshal(file, &cred)
+
+	conf = &oauth2.Config{
+		ClientID:     cred.Cid,
+		ClientSecret: cred.Csecret,
+		RedirectURL:  "https://red.bomaidea.xyz/todoAPI/v1/oauth",
+		Scopes: []string{
+			"https://www.googleapis.com/auth/userinfo.email", // You have to select your own scope from here -> https://developers.google.com/identity/protocols/googlescopes#google_sign-in
+			"https://www.googleapis.com/auth/userinfo.profile",
+		},
+		Endpoint: google.Endpoint,
+	}
+}
+
+func indexHandler(c *gin.Context) {
+	c.HTML(http.StatusOK, "index.tmpl", gin.H{})
+}
+
+func getLoginURL(state string) string {
+	return conf.AuthCodeURL(state)
+}
+
+func authHandler(c *gin.Context) {
+	// Handle the exchange code to initiate a transport.
+	//session := sessions.Default(c)
+	//retrievedState := session.Get("state")
+	//f retrievedState != c.Query("state") {
+	//	c.AbortWithError(http.StatusUnauthorized, fmt.Errorf("Invalid session state: %s", retrievedState))
+	//return
+	//}
+
+	tok, err := conf.Exchange(oauth2.NoContext, c.Query("code"))
+	if err != nil {
+		c.AbortWithError(http.StatusBadRequest, err)
+		return
+	}
+
+	client := conf.Client(oauth2.NoContext, tok)
+	email, err := client.Get("https://www.googleapis.com/oauth2/v3/userinfo")
+	if err != nil {
+		c.AbortWithError(http.StatusBadRequest, err)
+		return
+	}
+	defer email.Body.Close()
+	data, _ := ioutil.ReadAll(email.Body)
+	log.Println("Email body: ", string(data))
+	c.Status(http.StatusOK)
+}
+
+func loginHandler(c *gin.Context) {
+	//state = randToken()
+	//session := sessions.Default(c)
+	//session.Set("state", state)
+	//session.Save()
+	//c.Writer.Write([]byte("<html><title>Golang Google</title> <body> <a href='" + getLoginURL(state) + "'><button>Login with Google!</button> </a> </body></html>"))
+	c.JSON(http.StatusOK, gin.H{"url": getLoginURL(state)})
+}
+
+func main() {
+	router := gin.Default()
+	router.Use(sessions.Sessions("goquestsession", store))
+	router.LoadHTMLGlob("templates/*")
+
+	router.GET("/todoAPI", indexHandler)
+	router.GET("/todoAPI/login", loginHandler)
+	router.GET("/todoAPI/v1/oauth", authHandler)
+
+	router.Run("10.8.0.2:8080")
+}
